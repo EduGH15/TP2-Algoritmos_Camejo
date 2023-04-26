19e32b7218c07c255ad3ab97a59dbf47b7da8d7b
EduGH15 <eduardogl15.personal@gmail.com>
2023-04-25 23:32:06 -0300

    Agrego la biblioteca chambuchito.c aunque falta definir las constantes acá

---
+++ chambuchito.h
@@ -0,0 +1,10 @@
+#ifndef __CHAMBUCHITO_H__
+#define __CHAMBUCHITO_H__
+
+/*
+Pre:-----------------------------------
+Post:Devuelve el precio del chambuchito.
+*/
+void calcular_precio_chambuchito (int* precio);
+
+#endif /*__CHAMBUCHITO_H__*/
