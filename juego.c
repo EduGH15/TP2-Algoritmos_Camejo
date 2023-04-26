1816dc2f38f65f7a800924ddb9f0b0b31dadbf1a
EduGH15 <eduardogl15.personal@gmail.com>
2023-04-25 23:39:14 -0300

    Agrego una primera versión de juego.c que contiene un main que ejecuta el TP1

---
+++ juego.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "cocineritos.h"
+#include "chambuchito.h"
+
+int main(){
+    //int precio = 0;
+    //calcular_precio_chambuchito(&precio);
+    return 0;
+}
