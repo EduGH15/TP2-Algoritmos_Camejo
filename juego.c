b68ad288205c1dfbbc21b34f9f3ae49bd338eb4d
EduGH15 <eduardogl15.personal@gmail.com>
2023-04-29 17:43:33 -0300

    Agregué la ejecución de las funciones inicializar_juego e imprimir_terreno

--- juego.c
+++ juego.c
@@ -5,7 +5,10 @@
 #include "chambuchito.h"
 
 int main(){
-    //int precio = 0;
+    int precio = 0;
+    juego_t juego;
     //calcular_precio_chambuchito(&precio);
+    inicializar_juego(&juego, precio);
+    imprimir_terreno(juego);
     return 0;
 }
