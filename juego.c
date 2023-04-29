923ca5df764ca249f3567c06d1964e4fa942f77f
EduGH15 <eduardogl15.personal@gmail.com>
2023-04-29 19:31:24 -0300

    Le agrego los directorios necesarios para hacer numeros aleatorios

--- juego.c
+++ juego.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "cocineritos.h"
 #include "chambuchito.h"
 
 int main(){
+    srand (( unsigned)time(NULL));
+
     int precio = 0;
     juego_t juego;
     //calcular_precio_chambuchito(&precio);
